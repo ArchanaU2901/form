5f20bc2e872d82ec7de9370068d852c3b7fab4b9
Archana Upadhyay <archua1229@gmail.com>
2023-01-20 11:54:18 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css'
+import ContactUs from './Component/ContactUs';
+// import Contact from './Component/Contact';
+
+function App() {
+
+
+  return (
+    <div>
+        {/* <Contact/> */}
+        <ContactUs/>
+    </div>
+
+
+
+  )
+}
+
+export default App
---
+++ src/Component/ContactUs.jsx
@@ -0,0 +1,147 @@
+import React from 'react';
+// import 'bootstrap/dist/css/bootstrap.css';
+// import Row from 'react-bootstrap/Row';
+  
+export default function App() {
+  return (
+    <div >
+    <div class="container">
+    <h1>Form</h1>
+      <table class="rwd-table">
+        <tbody>
+          
+          <tr>
+           
+            <td data-th="Invoice Date">
+              Text
+            </td>
+            <td data-th="Due Date">
+              Text
+            </td>
+            <td data-th="Net Amount">
+             Files
+            </td>
+          </tr>
+          <tr>
+            
+            <td data-th="Invoice Date">
+            Text
+            </td>
+            <td data-th="Due Date">
+            Text
+            </td>
+            <td data-th="Net Amount">
+            Files
+            </td>
+          </tr>
+          <tr>
+            
+            <td data-th="Invoice Date">
+            Text
+            </td>
+            <td data-th="Due Date">
+            Text
+            </td>
+            <td data-th="Net Amount">
+            Files
+            </td>
+          </tr>
+          <tr>
+            
+            <td data-th="Invoice Date">
+            Text
+            </td>
+            <td data-th="Due Date">
+            Text
+            </td>
+            <td data-th="Net Amount">
+            Files
+            </td>
+          </tr>
+
+          <tr>
+            
+            <td data-th="Invoice Date">
+            Text
+            </td>
+            <td data-th="Due Date">
+            Text
+            </td>
+            <td data-th="Net Amount">
+            Files
+            </td>
+          </tr>
+
+          <tr>
+            
+            <td data-th="Invoice Date">
+            Text
+            </td>
+            <td data-th="Due Date">
+            Text
+            </td>
+            <td data-th="Net Amount">
+            Files
+            </td>
+          </tr>
+
+          <tr>
+            
+            <td data-th="Invoice Date">
+            Text
+            </td>
+            <td data-th="Due Date">
+            Text
+            </td>
+            <td data-th="Net Amount">
+            Files
+            </td>
+          </tr>
+
+          <tr>
+            
+            <td data-th="Invoice Date">
+                <input type="text" />
+            Text
+            </td>
+            <td data-th="Due Date">
+            <input type="text" placeholder='Text'/>
+            Text
+            </td>
+            <td data-th="Net Amount">
+            Files
+            </td>
+          </tr>
+
+          <tr>
+            
+            <td data-th="Invoice Date">
+            Text
+            </td>
+            <td data-th="Due Date">
+            Text
+            </td>
+            <td data-th="Net Amount">
+            Files
+            </td>
+          </tr>
+
+          <tr>
+            
+            <td data-th="Invoice Date">
+            Text
+            </td>
+            <td data-th="Due Date">
+            Text
+            </td>
+            <td data-th="Net Amount">
+            Files
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <h3>Submit</h3>
+    </div>
+    </div>
+  );
+}
